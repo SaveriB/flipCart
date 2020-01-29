0437364410a20e784f29fd92a8b059d42a53fbdf
saveri <985593@TCSNSN.COM>
2020-01-29 15:41:34 +0530

    removed dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+const serviceURL = "http://localhost:3000/store";
 export const environment = {
-  production: true
+  production: true,
+  Add_Product_In_Store : `${serviceURL}/product`,
+  Get_Product_In_Store : `${serviceURL}/product`,
+  Delete_Product_In_Store : `${serviceURL}/product`
 };
